30f736104b0c99d3e4327d70f99fcff7954577b4
mahdi manouchehri <mahdimanouchehry14@gmail.com>
2024-03-31 00:35:23 +0430

    debug mbaf file

--- measure_Binding_affinity_fragments.py
+++ measure_Binding_affinity_fragments.py
@@ -24,42 +24,41 @@ lig_coor.write_pdb('data/lig.pdb')
 rec_coor = coor_1hsg.select_part_dict(selec_dict={'res_name': pdb_manip.PROTEIN_RES})
 rec_coor.write_pdb('data/rec.pdb')
 
-
-
+fragments = {}
 
 for i in range(len(df)):
-  fragment_list = df["fragments"][0].split(".")
-  j = 1
-  for fragment in fragment_list:
-    mol = Chem.MolFromSmiles(fragment)
+    fragment_list = df["fragments"][i].split(".")
+    j = 1
+    for fragment in fragment_list:
 
-    # Add hydrogens to the molecule
-    mol = Chem.AddHs(mol)
+        if fragment not in fragments:
+            mol = Chem.MolFromSmiles(fragment)
 
-    # Generate 3D conformers
-    AllChem.EmbedMolecule(mol)
+            # Add hydrogens to the molecule
+            mol = Chem.AddHs(mol)
 
-    # Save the molecule to a PDB file
-    rdmolfiles.MolToPDBFile(mol, f"output{i}{j}.pdb")
+            # Generate 3D conformers
+            AllChem.EmbedMolecule(mol)
 
+            # Save the molecule to a PDB file
+            rdmolfiles.MolToPDBFile(mol, f"output{i}{j}.pdb")
 
+            test_dock = docking.Docking('test', lig_pdb=f"output{i}{j}.pdb", rec_pdb='data/rec.pdb')
+            test_dock.prepare_ligand()
+            test_dock.prepare_receptor()
 
-#    df[f"binding_affinity_frag{j}"][i] = binding_affinity1
+            test_dock.run_docking(out_pdb=f'test_dock{i}{j}.pdb',
+                                  num_modes=10,
+                                  energy_range=10,
+                                  exhaustiveness=16,
+                                  dock_bin='smina')
+            print(test_dock.affinity)
+            fragments[fragment] = test_dock.affinity[1]["affinity"]
+            df.loc[i, f"binding_affinity_frag{j}"] = test_dock.affinity[1]["affinity"]
 
-    test_dock = docking.Docking('test', lig_pdb=f"output{i}{j}.pdb", rec_pdb='data/rec.pdb')
-    test_dock.prepare_ligand()
-    test_dock.prepare_receptor()
-
-    test_dock.run_docking(out_pdb=f'test_dock{i}{j}.pdb',
-                      num_modes=10,
-                      energy_range=10,
-                      exhaustiveness=16,
-                      dock_bin='smina')
-    print(test_dock.affinity)
-    df.loc[i, f"binding_affinity_frag{j}"] = test_dock.affinity[1]["affinity"]
-
-    j += 1
-
+        else:
+            df.loc[i, f"binding_affinity_frag{j}"] = fragments[fragment]
+        j += 1
 
 df.to_csv("geom_multifrag_test_table_with_binding_affinity.csv")
 
